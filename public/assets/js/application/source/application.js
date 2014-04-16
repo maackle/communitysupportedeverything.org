c2f325842301b30e1b41c1521ace0ae3d44b1972
SamSmith17 <notsamsmith@gmail.com>
2014-04-16 14:03:21 -0700

    sam's changes

--- public/assets/js/application/source/application.js
+++ public/assets/js/application/source/application.js
@@ -1,52 +1,28 @@
-$(document).ready(function(){
-    /**
-    *   Global variables.
-    */
+$(document).ready(function() {
     var pageHeight = $(window).height();
     var pageWidth = $(window).width();
     var navigationHeight = $("#navigation").outerHeight();
-    
-    /**
-    *   ON RESIZE, check again
-    */
-    $(window).resize(function () {
+    $(window).resize(function() {
         pageWidth = $(window).width();
         pageHeight = $(window).height();
     });
-    
-    
-    /**
-    *   ON LOAD
-    */
-
-    /* Initialize scroll so if user droped to other part of page then home page. */
     $(window).trigger('scroll');
-    
-    /* Fix navigation. */
     $('#navigation').fixedonlater({
         speedDown: 250,
         speedUp: 100
     });
-    
-    /* Centralize elements on page. */
     $('.centralized').centralized({
         delay: 1500,
         fadeSpeed: 500
     });
-    
-    /* Make embeded videos responsive. */
     $.fn.responsivevideos();
-    
-    /* Carousel "Quote slider" initialization. */
-    $('#quote-slider').each(function(){
-        if($('.item', this).length) {
+    $('#quote-slider').each(function() {
+        if ($('.item', this).length) {
             $(this).carousel({
                 interval: 20000
             });
         }
     });
-    
-    /* Scroll spy and scroll filter */
     $('#main-menu').onePageNav({
         currentClass: "active",
         changeHash: false,
@@ -55,33 +31,23 @@ $(document).ready(function(){
         scrollSpeed: 750,
         filter: "",
         easing: "swing"
-     });
-    
-    /* 
-    *  Paralax initialization.
-    *  Exclude for mobile.
-    */
-    if(pageWidth > 980){
-        /* Dont user paralax for tablet and mobile devices. */
+    });
+    if (pageWidth > 980) {
         $('#page-welcome').parallax("0%", 0.2);
+//        $('#image-rotator img').parallax("0%", 0.07);
         $('#page-features').parallax("0%", 0.07);
         $('#page-twitter').parallax("0%", 0.1);
     }
-    
-    /* Emulate touch on table/mobile touchstart. */
-    if(typeof(window.ontouchstart) != 'undefined') {
+    if (typeof(window.ontouchstart) != 'undefined') {
         var touchElements = [".social-icons a", ".portfolio-items li", ".about-items .item"];
-        
-        $.each(touchElements, function (i, val) {
+        $.each(touchElements, function(i, val) {
             $(val).each(function(i, obj) {
-                $(obj).bind('click', function(e){
-                
-                    if($(this).hasClass('clickInNext')){
+                $(obj).bind('click', function(e) {
+                    if ($(this).hasClass('clickInNext')) {
                         $(this).removeClass('clickInNext');
                     } else {
                         e.preventDefault();
                         e.stopPropagation();
-                        
                         $(this).mouseover();
                         $(this).addClass('clickInNext');
                     }
@@ -89,53 +55,25 @@ $(document).ready(function(){
             });
         });
     }
-
-    /**
-    *   BLOCK | Navigation
-    *
-    *   Smooth scroll
-    *   Main menu links
-    *   Logo click on Welcome page
-    */
-    $('#page-welcome .logo a').click(function(){
+    $('#page-welcome .logo a').click(function() {
         $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top - navigationHeight + 4
+            scrollTop: $($.attr(this, 'href')).offset().top - navigationHeight + 4
         }, 800);
-        
-        /* Fix jumping of navigation. */
         setTimeout(function() {
             $(window).trigger('scroll');
         }, 900);
-        
         return false;
     });
-    
-    /**
-    *   PAGE | Welcome 
-    *
-    *   Initialize slider for welcome page H1 message.
-    */
-   /*
-$('#welcome-messages ul').bxSlider({
-        mode: 'vertical',
-        auto: true,
-        minSlides: 1,
-        responsive: true,
-        touchEnabled: true,
-        pager: false,
-        controls: false,
-        useCSS: false,
-        pause: 10000
+/*
+    var $imageDiv = $("#image-rotator");
+    var $imageRotator = $imageDiv.imageRotator({
+        imageTime: 6000,
+        fadeTime: 4000
     });
+    $imageRotator.start();
 */
     
-    /**
-    *   PAGE | WORK
-    *
-    *   .plugin-filter - Defines action links.
-    *   .plugin-filter-elements - Defines items with li.
-    */
-    $('.plugin-filter').click(function(){
+    $('.plugin-filter').click(function() {
         return false;
     });
     $('.plugin-filter-elements').mixitup({
@@ -143,7 +81,7 @@ $('#welcome-messages ul').bxSlider({
         filterSelector: '.plugin-filter',
         sortSelector: '.sort',
         buttonEvent: 'click',
-        effects: ['fade','rotateY'],
+        effects: ['fade', 'rotateY'],
         listEffects: null,
         easing: 'smooth',
         layoutMode: 'grid',
@@ -166,59 +104,32 @@ $('#welcome-messages ul').bxSlider({
         onMixStart: null,
         onMixEnd: null
     });
-    
-    /**
-    *   PAGE | Twitter 
-    *
-    *   Pull latest tweets from user.
-    *   Configuration: /plugins/twitter/index.php
-    */
     $('#twitterfeed-slider').tweet({
         modpath: 'plugins/twitter/',
         username: 'TheGridelicious',
         count: 3
     });
-    
     $('#twitterfeed-slider').tweetCarousel({
         interval: 7000,
         pause: "hover"
     });
 });
-
-
-/**
-*   Ajax request.
-*   Start loading.
-*   Append loading notification.
-*/
-$( document ).ajaxSend( function() {
-    /* Show loader. */
-    if($(".loading").length == 0) {
+$(document).ajaxSend(function() {
+    if ($(".loading").length == 0) {
         $("body").append('<div class="loading"><div class="progress progress-striped active"><div class="bar"></div></div></div>');
         $(".loading").slideDown();
         $(".loading .progress .bar").delay(300).css("width", "100%");
     }
 });
-
-/**
-*   Reinitialize Scrollspy after ajax request is completed.
-*   Refreshing will recalculate positions of each page in document.
-*   Time delay is added to allow ajax loaded content to expand and change height of page.
-*/
-$( document ).ajaxComplete(function() {
-    /* Remove loading section. */
-    $(".loading").delay(1000).slideUp(500, function(){
+$(document).ajaxComplete(function() {
+    $(".loading").delay(1000).slideUp(500, function() {
         $(this).remove();
     });
-    
-    /* Portfolio details - close. */
     $(".close-portfolio span").click(function(e) {
-        $(".portfolio-item-details").delay(500).slideUp(500, function(){
+        $(".portfolio-item-details").delay(500).slideUp(500, function() {
             $(this).remove();
         });
-        
-        window.location.hash= "!";
+        window.location.hash = "!";
         return false;
     });
 });
-
