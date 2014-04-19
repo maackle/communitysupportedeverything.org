a05213c877ec2cf243868bc1a1d8d2074465b9e8
Michael Dougherty <maackle.d@gmail.com>
2014-04-19 16:47:21 -0700

    trying some heroku nonsense

--- application.py
+++ application.py
@@ -53,8 +53,8 @@ def home():
 
 
 if __name__ == '__main__':
-	handler = RotatingFileHandler("log/error.log", maxBytes=10000000, backupCount=10)
-	handler.setLevel(logging.WARNING)
-	app.logger.addHandler(handler)
+	# handler = RotatingFileHandler("log/error.log", maxBytes=10000000, backupCount=10)
+	# handler.setLevel(logging.WARNING)
+	# app.logger.addHandler(handler)
 
 	app.run(debug=True, port=5005)
