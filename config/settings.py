18f742a88e8dd59f3fd8e6fec648dedfc8590ab0
Michael Dougherty <maackle.d@gmail.com>
2014-04-19 16:16:49 -0700

    updated stuff for heroku

--- config/settings.py
+++ config/settings.py
@@ -4,5 +4,6 @@ CSRF_ENABLED = True
 WTF_CSRF_ENABLED = True
 
 MONGODB_SETTINGS = {
-	'DB': 'cse'
+	'DB': 'cse',
+	'host': 'mongodb://heroku_app24328503:qhgcm6n7d59930tar1evhp6q3s@ds053708.mongolab.com:53708/heroku_app24328503',
 }
