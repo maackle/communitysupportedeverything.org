0ee269ca2c968479a1a17fed8d110000d201aba2
Michael Dougherty <maackle.d@gmail.com>
2014-04-29 12:39:22 -0700

    updated production

--- config/production.py
+++ config/production.py
@@ -1,5 +1,6 @@
 SECRET_KEY = 'LKSDHF*(*(98GG87W]\|}|CNJ'
 
 MONGODB_SETTINGS = {
+	'DB': 'cse',
 	'host': 'mongodb://heroku_app24328503:qhgcm6n7d59930tar1evhp6q3s@ds053708.mongolab.com:53708/heroku_app24328503',
 }
