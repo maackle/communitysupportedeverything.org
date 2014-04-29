aafa3930c97c4fc7c57238220f826f159bada6bf
Michael Dougherty <maackle.d@gmail.com>
2014-04-29 12:36:59 -0700

    screw it...let's use production config by default for now

--- application.py
+++ application.py
@@ -24,8 +24,8 @@ def create_app():
 		app.config.from_envvar('CSE_SETTINGS')
 		print("Loaded config from envvar CSE_SETTINGS")
 	except:
-		app.config.from_object('config.development')
-		print("Loaded DEVELOPMENT config")
+		app.config.from_object('config.production')
+		print("Loaded PRODUCTION config")
 
 	app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
